3aa0dea0579926d8286f0ea32d258f39940c56fe
irakli1i <113178329+irakli1i@users.noreply.github.com>
2022-11-01 07:11:16 -0700

    davalebaoop

---
+++ javascript.js
@@ -0,0 +1,87 @@
+// var cinema = {
+//     name:"ამირანი",
+//     languages : ["ქართული","ინგლისური","იტალიური"],
+//     Address : "თბილისი",
+//     movie : {
+//         name : "Halloween",
+//         year: "1987",
+//         imdb : "9.9" 
+//     },
+//     other:[
+//         {name:"It Follows", year:"2014", imdb:"6.8"},
+//         {name:"The Thing", year:"1982", imdb:"8.2"},
+//         {name:"Nosferatu", year:"1922", imdb:"7.9"},
+//         {name:"Young Frankenstein", year:"1974", imdb:"8"}
+//     ]
+// }
+// document.write("<h2>ფილმები<h2>")
+// for (var i=0; i < cinema.other.length; i++){
+//     document.write(cinema. other [i].name + "</br>")
+// }
+
+// var cinema = {
+//     name:"ამირანი",
+//     languages : ["ქართული","ინგლისური","იტალიური"],
+//     Address : "თბილისი",
+//     movie : {
+//         name : "Halloween",
+//         year: "1987",
+//         imdb : "9.9" 
+//     },
+//     other:[
+//         {name:"It Follows", year:"2014", imdb:"6.8"},
+//         {name:"The Thing", year:"1982", imdb:"8.2"},
+//         {name:"Nosferatu", year:"1922", imdb:"7.9"},
+//         {name:"Young Frankenstein", year:"1974", imdb:"8"}
+//     ]
+// }
+// document.write("<h2>ფილმები<h2>")
+// for (var i=0; i < cinema.other.length; i++){
+//     document.write(cinema. other [i].name + "</br>")
+//     document.write(cinema. other [i].year + "</br>")
+//     document.write(cinema. other [i].imdb + "</br>")
+// }
+    
+
+// es ver gavakete
+// var cinema = {
+//     name:"ამირანი",
+//     languages : ["ქართული","ინგლისური","იტალიური"],
+//     Address : "თბილისი",
+//     movie : {
+//         name : "Halloween",
+//         year: "1987",
+//         imdb : "9.9" 
+//     },
+//     other:[
+//         {name:"It Follows", year:"2014", imdb:"6.8"},
+//         {name:"The Thing", year:"1982", imdb:"8.2"},
+//         {name:"Nosferatu", year:"1922", imdb:"7.9"},
+//         {name:"Young Frankenstein", year:"1974", imdb:"8"}
+//     ]
+// }
+// document.write("<h2>ფილმები<h2>")
+// for (var i=0; i < cinema.movie.length; i++){
+//     document.write(cinema.movie.name [i] )
+// }
+
+// var cinema = {
+//     name:"ამირანი",
+//     languages : ["ქართული","ინგლისური","იტალიური"],
+//     Address : "თბილისი",
+//     movie : {
+//         name : "Halloween",
+//         year: "1987",
+//         imdb : "9.9" 
+//     },
+//     other:[
+//         {name:"It Follows", year:"2014", imdb:"6.8"},
+//         {name:"The Thing", year:"1982", imdb:"8.2"},
+//         {name:"Nosferatu", year:"1922", imdb:"7.9"},
+//         {name:"Young Frankenstein", year:"1974", imdb:"8"}
+//     ]
+// }
+// document.write("<h2>ფილმები<h2>")
+// for (var i=0; i < cinema.languages.length; i++){
+//     document.write(cinema.languages [i] + "</br>")
+// }
